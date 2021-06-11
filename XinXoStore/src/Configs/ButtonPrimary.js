c1c9622bb979d9c06ac2aed05266f2a51f86d782
Thuyety <thuyety15@gmail.com>
2021-06-11 16:52:44 +0700

    component button

---
+++ XinXoStore/src/Configs/ButtonPrimary.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { StyleSheet , Text, View, TouchableOpacity} from 'react-native';
+
+export default class ButtonPrimary extends React.Component{
+    render(){
+        return(
+            <View style={styles.ButtonSignUp}>
+                <TouchableOpacity  >
+                    <Text
+                        style={styles.TextSignUp}>
+                        Sign Up
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+}
+const styles=StyleSheet.create({
+    TextSignUp:{
+        color:'#fff',
+        textAlign: 'center', 
+        fontSize: 20, 
+        paddingVertical: 15,
+        fontWeight:'bold',
+        letterSpacing:1
+    },
+    ButtonSignUp:{
+        backgroundColor:'#638adf',
+        borderWidth:0,
+        borderRadius:30,
+        marginTop:15,
+        marginBottom:15
+    }
+});
--- XinXoStore/src/routes/screens/SignUpScreen.js
+++ XinXoStore/src/routes/screens/SignUpScreen.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import {View,Text,Image,StyleSheet,TextInput, Button,TouchableOpacity} from 'react-native';
+import ButtonPrimary from '../../Configs/ButtonPrimary';
+import ButtonExtension from '../../Configs/ButtonExtension';
+import LogoGoogle from '../../images/logo/google.png';
+import LogoFacebook from '../../images/logo/facebook.png';
 
 export default class SignUpScreen extends React.Component{
-    // constructor(prop){
-    //     super(props);
-    //     this.state={
-    //         username:'',
-    //         password:'',
-    //         email:''
-    //     }
-    // }
-
+    constructor(props){
+        super(props);
+        this.state={
+            username:'',
+            password:'',
+            email:''
+        }
+    }
+    textChangeHandler(value,type){
+        switch(type){
+            case 'username':
+                this.setState({username:value});
+                break;
+            case 'password':
+                this.setState({password:value});
+                break;
+            case 'email':
+                this.setState({email:value});
+        }
+    }
+    textOutput(){
+        return (
+            this.state.username+' ' +this.state.email+' '+ this.state.password
+        );
+    }
     render(){
         return(
             <View style={{backgroundColor:'#fcf8ee',height:'100%'}}>
@@ -18,20 +38,19 @@ export default class SignUpScreen extends React.Component{
                 <View style={styles.Container}>
                     <Text style={styles.HeaderText}>Sign Up</Text>
                     <View>
-                        <TextInput  style={styles.TextInput}
-                            placeholder="Email"/>
-                        <TextInput  style={styles.TextInput}
-                            placeholder="Password"/>
-                        <TextInput  style={styles.TextInput}
-                            placeholder="Phone"/>
-                        <View style={styles.ButtonSignUp}>
-                            <TouchableOpacity  >
-                                <Text
-                                    style={styles.TextSignUp}>
-                                    Sign Up
-                                </Text>
-                            </TouchableOpacity>
-                        </View>
+                        <TextInput  
+                            style={styles.TextInput}
+                            placeholder="Username"
+                            onChangeText={value=>this.textChangeHandler(value,'username')}/>
+                        <TextInput  
+                            style={styles.TextInput}
+                            placeholder="Email"
+                            onChangeText={value=>this.textChangeHandler(value,'email')}/>
+                        <TextInput  
+                            style={styles.TextInput}
+                            placeholder="Password"
+                            onChangeText={value=>this.textChangeHandler(value,'password')}/>
+                        
                         <View style={styles.TextPolicy}>
                             <Text 
                                 style={{color:'gray'}}>
@@ -42,30 +61,17 @@ export default class SignUpScreen extends React.Component{
                                 Terms and Conditions
                             </Text>
                         </View>
-                        
+                        <ButtonPrimary />
                     </View>               
                 </View>
-
-                {/* sign up with google or facebook */}
+                 {/* sign up with google or facebook */}
                 <View>
                     <View style={styles.ExtensionContainer}>
-                            <TouchableOpacity style={styles.ButtonExtension} >
-                                <Image style={{height:20,width:20}} source={require('../../images/logo/google.png')} />
-                                <Text
-                                    style={styles.TextExtension}>
-                                    Google
-                                </Text>
-                            </TouchableOpacity>
-                            <TouchableOpacity style={styles.ButtonExtension} >
-                                <Image style={{height:20,width:20}} source={require('../../images/logo/facebook.png')} />
-                                <Text
-                                    style={styles.TextExtension}>
-                                    Facebook
-                                </Text>
-                            </TouchableOpacity>
+                            <ButtonExtension text="Google" logo={LogoGoogle}/>
+                            <ButtonExtension text="Facebook" logo={LogoFacebook}/>
                     </View>
                 </View>
-                {/* sign in link */}
+                {/* sign in link  */}
                 <View style={styles.SignInLink}>
                     <Text style={styles.GrayText}>
                         Already have an account?  
@@ -74,6 +80,9 @@ export default class SignUpScreen extends React.Component{
                         Sign in
                     </Text>
                 </View>
+                <Text>
+                    result: {this.textOutput()}
+                </Text>
             </View>
         );
     }
@@ -111,48 +120,9 @@ const styles=StyleSheet.create({
         borderBottomColor: '#d2d2d2',
         marginBottom:15
     },
-    TextSignUp:{
-        color:'#fff',
-        textAlign: 'center', 
-        fontSize: 20, 
-        paddingVertical: 15,
-        fontWeight:'bold',
-        letterSpacing:1
-    },
-    ButtonSignUp:{
-        backgroundColor:'#638adf',
-        borderWidth:0,
-        borderRadius:30,
-        marginTop:15,
-        marginBottom:15,
-        
-    },
     TextPolicy:{
         alignItems:'center',
         marginBottom:20
-    },
-    //extension
-    TextExtension:{
-        color:'gray',
-        fontWeight:'bold',
-        fontSize:18,
-        textAlign: 'center', 
-        paddingVertical: 15,
-        fontWeight:'bold',
-        letterSpacing:1,
-        marginHorizontal:10
-    },
-    ButtonExtension:{
-        backgroundColor:'#fff',
-        borderWidth:0,
-        borderRadius:30,
-        marginTop:15,
-        marginBottom:15,
-        display:'flex',
-        alignItems:'center',
-        flexDirection:'row',
-        justifyContent:'center',
-        flex:1
     },
     ExtensionContainer:{
         display:'flex',
@@ -164,6 +134,5 @@ const styles=StyleSheet.create({
         display:'flex',
         justifyContent:'center',
         flexDirection:'row',
-        
     }
 });
