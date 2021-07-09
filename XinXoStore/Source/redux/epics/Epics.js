43a00fd5ab710424835966e0482dc965954fca9c
Thuyety <thuyety15@gmail.com>
2021-07-09 16:29:20 +0700

    fix after merge shopping cart & master

--- XinXoStore/Source/redux/epics/Epics.js
+++ XinXoStore/Source/redux/epics/Epics.js
@@ -3,7 +3,6 @@ import NewArrivalsEpic from './NewArrivalsEpics/NewArrivalsEpics';
 import DetailArrivalEpic from './DetailEpics/DetailArrivalEpic';
 import SignUpEpic from './SignUpEpics/SignUpEpic';
 import { combineEpics } from 'redux-observable';
-import NewArrivalsEpic from './NewArrivalsEpics/NewArrivalsEpics';
 import AdressEpic from './AdressEpics/AdressEpic';
 import ShoppingCartEpic from './ShoppingCartEpics/ShoppingCartEpic';
 
